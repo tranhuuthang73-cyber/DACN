a50bec30c1d6a28adcac45d7144de9c17bf96632
Trần Thái Dương <tranhuuthang73@gmail.com>
2026-08-28 21:10:09 +0700

    Upload PUSH_TO_GITHUB.bat

---
+++ PUSH_TO_GITHUB.bat
@@ -0,0 +1,13 @@
+@echo off
+title TravelGo - Day Source Code len GitHub
+color 0A
+cls
+echo ===================================================================
+echo             TRAVELGO - GITHUB UPLOADER (REPO: DACN)
+echo         https://github.com/tranhuuthang73-cyber/DACN
+echo ===================================================================
+echo.
+echo [*] Dang khoi chay bo dong bo truc tiep len GitHub...
+echo.
+node scripts\github_sync.js
+pause
