349f8d8b28247f203a98fa85c4c07c3f569e4c13
Trần Thái Dương <tranhuuthang73@gmail.com>
2026-08-28 21:17:48 +0700

    Remove deprecated folder: backend/src/routes/plotLocationAdvisor.ts

--- backend/src/routes/plotLocationAdvisor.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { analyzePlotLocation } from '../controllers/plotLocationAdvisor';
-
-const router = Router();
-
-router.get('/analyze', analyzePlotLocation);
-
-export default router;
